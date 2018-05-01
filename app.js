8f8e2a54291f721588075254a838f94aa39b871f
kalle <kmthorsnes@gmail.com>
2018-05-01 19:32:25 +0200

    add console.log test and refactors index.html

--- app.js
+++ app.js
@@ -21,3 +21,25 @@ var cpuChoice = {
 
 cpuChoice.init();
 console.log('cpuchoice:', cpuChoice.store, cpuChoice.text);
+
+
+// Declare a winner
+
+var order = [0, 1, 2, 0];
+
+// adds conditionals
+
+var chooseWinner = function(player, cpu){
+  if(order[player] === order[cpu]) {
+    return 'Tied game. Try again?';
+  }
+  if(order[player] === order[cpu + 1]) {
+    score++;
+    return "Hooray, you are the winner!";
+  } else {
+    score--;
+    return "Bummer! You lost this time";
+  }
+}
+
+console.log(chooseWinner(2, 0));
